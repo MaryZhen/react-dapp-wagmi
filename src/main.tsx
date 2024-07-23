8e77253cd4a3499dc1017d2b3717fe312405d277
Eva <31976043+MaryZhen@users.noreply.github.com>
2024-07-23 10:59:01 +0800

    使用wagmi实现连接钱包功能
    使用wagmi实现连接钱包功能

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+  darkTheme
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+  sepolia
+} from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
 
+const config = getDefaultConfig({
+  appName: 'My Wagmi Project',
+  projectId: '9320576b52fea108db7634255b1cd746',
+  chains: [mainnet, polygon, optimism, arbitrum, base, sepolia],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider theme={darkTheme()}>
+          <App />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>, 
 )
