0c86b7b0b17d61215d202f43fb0488e51cd8bf9d
Eva <31976043+MaryZhen@users.noreply.github.com>
2024-07-24 12:55:19 +0800

    Delete complain
    Delete complain

--- src/router/ErrorPage.tsx
+++ src/router/ErrorPage.tsx
@@ -1,17 +1,8 @@
-// import { useRouteError } from "react-router-dom";
-
 export default function ErrorPage() {
-//   const error: any = useRouteError();
-//   console.error(error);
-//   const textStr = error?.statusText || error?.message || ''
-
   return (
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
-      {/* <p>
-        <i>{ textStr }</i>
-      </p> */}
     </div>
   );
 }
